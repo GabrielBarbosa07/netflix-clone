d97ac660d99d8932108da9a4c29a1dd57bd88c40
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2023-06-08 20:56:02 -0300

    Navbar pronta

---
+++ src/app/components/AccountMenu.tsx
@@ -0,0 +1,40 @@
+import { signOut, useSession } from "next-auth/react";
+
+import Image from "next/image";
+
+import defaultBlue from "../../../public/images/default-blue.png";
+
+interface AccountMenuProps {
+  visible?: boolean;
+}
+
+const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  const { data: session } = useSession();
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="flex flex-col gap-3">
+        <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
+          <Image className="rounded-md" src={defaultBlue} alt="" width={32} />
+          <p className="text-white text-sm group-hover/item:underline">
+            {session?.user?.name}
+          </p>
+        </div>
+        <hr className="bg-gray-600 border-0 h-px my-4" />
+
+        <div
+          onClick={() => signOut({ callbackUrl: "/auth" })}
+          className="px-3 text-center text-white text-sm hover:underline"
+        >
+          Sair da Netflix
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountMenu;
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,44 @@
-import { useCallback, useState } from "react";
-
+import { useCallback, useEffect, useState } from "react";
 import Image from "next/image";
+
 import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
 
 import Logo from "../../../public/images/logo.png";
+import defaultBlue from "../../../public/images/default-blue.png";
+
 import NavbarItem from "./NavbarItem";
 import MobileMenu from "./MobileMenu";
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 88;
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
   }, []);
 
   const navbarTitles: string[] = [
@@ -25,7 +52,11 @@ const Navbar = () => {
 
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-12 py-6 md:px-16 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+      <div
+        className={`px-12 py-6 md:px-16 flex flex-row justify-center items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900 bg-opacity-90" : ""
+        }`}
+      >
         <Image src={Logo} alt="Logo" height={28} draggable={false} />
 
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
@@ -36,10 +67,14 @@ const Navbar = () => {
 
         <div
           onClick={toggleMobileMenu}
-          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+          className="lg:hidden flex flex-row items-center gap-2 mx-8 cursor-pointer relative"
         >
           <p className="text-white text-sm">Navegar</p>
-          <BsChevronDown className="text-white transition" />
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
           <MobileMenu visible={showMobileMenu} navbarTitles={navbarTitles} />
         </div>
 
@@ -50,6 +85,21 @@ const Navbar = () => {
 
           <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
             <BsBell />
+          </div>
+
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-3 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <Image src={defaultBlue} alt="" />
+            </div>
+            <BsChevronDown
+              className={`text-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
           </div>
         </div>
       </div>
