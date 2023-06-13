dda4f43da2e14ed7f8e90ab8d8b7ac70b752c403
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2023-06-13 13:27:09 -0300

    MovieCard quase pronto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Loading from "./loading";
 import Navbar from "./components/Navbar";
 import Billboard from "./components/Billboard";
+import MovieList from "./components/MovieList";
+import fetcher from "../../lib/fetcher";
 
 export default function Home() {
   const { push } = useRouter();
   const { data: session, status } = useSession();
+
+  const [movies, setMovies] = useState([]);
 
   {
     status !== "authenticated" && <Loading />;
@@ -19,12 +24,21 @@ export default function Home() {
     push("/auth");
   }
 
+  useEffect(() => {
+    fetcher("http://localhost:3000/api/movies").then((response) => {
+      setMovies(response);
+    });
+  }, []);
+
   return (
     <>
       {status === "authenticated" && (
         <>
           <Navbar />
-          <Billboard/>
+          <Billboard />
+          <div className="pb-40">
+            <MovieList title="Tendência Agora" data={movies} />
+          </div>
         </>
       )}
     </>
