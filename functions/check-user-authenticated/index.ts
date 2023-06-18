6a2b97f8a525ecf838138dd864ced4aab0bebaab
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2023-06-18 11:04:20 -0300

    Hook para a chamada de filmes favoritos feita

--- functions/check-user-authenticated/index.ts
+++
@@ -1,8 +0,0 @@
-import {cookies} from "next/headers"
-
-export const checkUserAuthenticated = () =>{
-    const cookieStore = cookies();
-    const userToken = cookieStore.get("next-auth.session-token")
-
-    return !!userToken
-}
